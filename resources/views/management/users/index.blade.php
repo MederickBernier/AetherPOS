16bb84d37b18b49f3e2f937d03c82aae0393d8aa
Mederick Bernier <mederick.bernier@hotmail.ca>
2023-10-09 13:14:26 -0400

    design wasn't working on medium devices

--- resources/views/management/users/index.blade.php
+++ resources/views/management/users/index.blade.php
@@ -11,41 +11,69 @@
         </div>
     </div>
 
-    <div class="user-list">
-        @if($users->count())
-            @foreach($users as $user)
-                <div class="user-item mb-3">
-                    <!-- User Name (Clickable) -->
-                    <div class="item-header">
-                        {{ $user->character_first_name }} {{ $user->character_last_name }}
-                        <span class="toggle-details">[+]</span>
-                    </div>
-
-                    <!-- User Details (Hidden initially) -->
-                    <div class="item-details" style="display: none;">
-                        <p><strong>ID:</strong> {{ $user->id }}</p>
-                        <p><strong>Email:</strong> {{ $user->email }}</p>
-                        <p><strong>Server:</strong> {{ $user->character_server }}</p>
-                        <p><strong>Last Active:</strong> {{ $user->last_active ? $user->last_active->diffForHumans() : 'N/A' }}</p>
-                        <div>
-                            <a href="{{ route('management.users.edit', $user->id) }}" class="btn btn-primary btn-sm">Edit</a>
-                            <a href="{{ route('management.users.show', $user->id) }}" class="btn btn-success btn-sm">View</a>
-                            <form action="{{ route('management.users.destroy', $user->id) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this user?')">Delete</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        @else
-            <div class="user-item">
-                <div class="item-header">
-                    No users found in the database.
+    <!-- Collapsible design for medium and smaller devices -->
+    <div class="user-list d-lg-none">
+        @foreach ($users as $user)
+        <div class="user-item">
+            <div class="item-header">
+                {{ $user->character_first_name }} {{ $user->character_last_name }}
+                <span class="toggle-details">[+]</span>
+            </div>
+            <div class="item-details" style="display: none;">
+                <p><strong>ID:</strong> {{ $user->id }}</p>
+                <p><strong>Email:</strong> {{ $user->email }}</p>
+                <p><strong>Server:</strong> {{ $user->character_server }}</p>
+                <p><strong>Last Active:</strong> {{ $user->last_active ? $user->last_active->diffForHumans() : 'N/A' }}</p>
+                <div>
+                    <a href="{{ route('management.users.edit', $user->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                    <a href="{{ route('management.users.show', $user->id) }}" class="btn btn-sm btn-success">View</a>
+                    <form action="{{ route('management.users.destroy', $user->id) }}" method="POST" class="d-inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this user?')">Delete</button>
+                    </form>
                 </div>
             </div>
-        @endif
+        </div>
+        @endforeach
+    </div>
+
+    <!-- Table design for large devices -->
+    <div class="d-none d-lg-block">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Server</th>
+                    <th>Last Active</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->character_first_name }}</td>
+                    <td>{{ $user->character_last_name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->character_server }}</td>
+                    <td>{{ $user->last_active ? $user->last_active->diffForHumans() : 'N/A' }}</td>
+                    <td>
+                        <a href="{{ route('management.users.edit', $user->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                        <a href="{{ route('management.users.show', $user->id) }}" class="btn btn-sm btn-success">View</a>
+                        <form action="{{ route('management.users.destroy', $user->id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this user?')">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 
