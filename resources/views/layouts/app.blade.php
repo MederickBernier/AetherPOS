3e1b0ce1c6e0c45844a8dd66dc16e9dc66281864
Mederick Bernier <mederick.bernier@hotmail.ca>
2023-10-12 21:50:38 -0400

    added a tooltip to the user name to go to profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,13 @@
 
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <script>
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+    </script>
+
 </body>
 
 </html>
